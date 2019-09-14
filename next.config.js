80b565e77c2a653dfebb18e720467afce5fc4f00
Brais Antela <braantela@gmail.com>
2019-09-14 18:14:44 +0200

    First Version

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  cssModules: true,
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+
+    config.node = {
+      fs: 'empty'
+    }
+
+    return config
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import { Button } from 'reactstrap'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap'
+
+function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div>
+      <Head title="Beyond" />
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <div>
+        <Button color="primary">New</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
